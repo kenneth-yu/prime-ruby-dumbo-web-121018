804ad0d24fc905d90b9104ca0cf7469d2ea5e4b1
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 18:41:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,13 @@
 # Add  code here!
 def prime? (number)
-  if number 
+  start = 2
+  primes = (start..number).to_a
+  (start..number).each do |no|
+    (start..no).each do |num|
+      if ( no % num  == 0) && num != no
+        primes.delete(no)
+        break
+      end
+    end
+  end
+  primes
